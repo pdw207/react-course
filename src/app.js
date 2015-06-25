f2da688e12ef557ae1489eb9c32f986196d7c234
Paul Wilson <paul@vermonster.com>
2015-06-25 12:36:08 -0400

    Setup simple webpage

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-document.write('Hello Frontend Masters!')
+import React from 'react'
+import Styles from './styles/main.styl'
+
+const Hello = React.createClass({
+  render () {
+    return <div> Hello, {this.props.name}</div>
+  }
+})
+
+React.render(<Hello name="mari"/>, document.body)
